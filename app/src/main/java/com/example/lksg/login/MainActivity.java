8a8ea1d6bb5718191f7b14d736d35e1d12da03dd
Jade Soo <jade_soo_123@hotmail.com>
2019-03-27 18:16:43 +1100

    Test Commit

--- app/src/main/java/com/example/lksg/login/MainActivity.java
+++ app/src/main/java/com/example/lksg/login/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // test
+    
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonLogin;
