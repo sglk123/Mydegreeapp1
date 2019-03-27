27b7cc70f61023d7cb315198e4c886759a37fce8
yaowang <ericyaowang@gmail.com>
2019-03-27 18:23:52 +1100

    This is Eric's Test

--- app/src/main/java/com/example/lksg/login/MainActivity.java
+++ app/src/main/java/com/example/lksg/login/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     // test
+
+    // Eric Test
     
     EditText mTextUsername;
     EditText mTextPassword;
